7b84487cff48f276c6081f1cedb52d2f6eccd213
sbleru <usareru@gmail.com>
2022-05-08 20:43:43 +0900

    feat: Add style with ChakraUI

--- apps/warikan-netflix/src/App.tsx
+++ apps/warikan-netflix/src/App.tsx
@@ -1,45 +1,75 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import logo from "./logo.svg";
+import { Button, Center, Heading, Image, Link, VStack } from "@chakra-ui/react";
+import { keyframes } from "@chakra-ui/react";
+
+const animationKeyframes = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const animation = `${animationKeyframes} infinite 20s linear`;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+    <Center
+      minH={"100vh"}
+      backgroundColor="#282c34"
+      display={"flex"}
+      flexDir={"column"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      fontSize={"xl"}
+      color={"white"}
+    >
+      <Image
+        src={logo}
+        alt="logo"
+        h={"40vmin"}
+        pointerEvents={"none"}
+        animation={animation}
+      />
+      <VStack spacing={"2"}>
+        <Heading>Hello Vite + React!</Heading>
+        <Heading>
+          <Button
+            onClick={() => setCount((count) => count + 1)}
+            color={"black"}
+          >
             count is: {count}
-          </button>
-        </p>
-        <p>
+          </Button>
+        </Heading>
+        <Heading>
           Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
+        </Heading>
+        <Heading>
+          <Link
+            color={"#61dafb"}
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
+          </Link>
+          {" | "}
+          <Link
+            color={"#61dafb"}
             href="https://vitejs.dev/guide/features.html"
             target="_blank"
             rel="noopener noreferrer"
           >
             Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+          </Link>
+        </Heading>
+      </VStack>
+    </Center>
+  );
 }
 
-export default App
+export default App;
