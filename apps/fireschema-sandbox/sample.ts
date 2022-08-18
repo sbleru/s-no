d9a18bb0fb523c096f30de3d33088fa56e2d1ca5
sbleru <usareru@gmail.com>
2022-08-18 21:11:04 +0900

    feat: Add fireschema-sandbox

---
+++ apps/fireschema-sandbox/sample.ts
@@ -0,0 +1,87 @@
+import { Merge } from "type-fest";
+import { z } from "zod";
+
+import { DataModel, FirestoreModel, rules, timestampType } from "fireschema";
+
+export const UserType = z.object({
+  name: z.string(),
+  displayName: z.union([z.string(), z.null()]),
+  age: z.number().int(),
+  timestamp: timestampType(),
+  options: z.object({ a: z.boolean() }).optional(),
+});
+
+type User = z.infer<typeof UserType>;
+/* => {
+  name: string
+  displayName: string | null
+  age: number
+  timestamp: FTypes.Timestamp
+  options?: { a: boolean } | undefined
+} */
+
+type UserDecoded = Merge<User, { timestamp: Date }>;
+
+const UserModel = new DataModel({
+  schema: UserType,
+  decoder: (data: User): UserDecoded => ({
+    ...data,
+    timestamp: data.timestamp.toDate(),
+  }),
+});
+
+const PostType = z.object({
+  authorUid: z.string(),
+  text: z.string(),
+  tags: z.object({ id: z.number().int(), name: z.string() }).array(),
+});
+
+const PostModel = new DataModel({
+  schema: PostType,
+  selectors: (q) => ({
+    byTag: (tag: string) => [
+      q.where("tags", "array-contains", tag),
+      q.limit(20),
+    ],
+  }),
+});
+
+export const firestoreModel = new FirestoreModel({
+  "function isAdmin()": `
+    return exists(${rules.basePath}/admins/$(request.auth.uid));
+  `,
+
+  "function requestUserIs(uid)": `
+    return request.auth.uid == uid;
+  `,
+
+  collectionGroups: {
+    "/posts/{postId}": {
+      allow: {
+        read: true,
+      },
+    },
+  },
+
+  "/users/{uid}": {
+    model: UserModel,
+    allow: {
+      read: true, // open access
+      write: rules.or("requestUserIs(uid)", "isAdmin()"),
+    },
+
+    "/posts/{postId}": {
+      "function authorUidMatches()": `
+        return request.resource.data.authorUid == uid;
+      `,
+
+      model: PostModel,
+      allow: {
+        read: true,
+        write: rules.and("requestUserIs(uid)", "authorUidMatches()"),
+      },
+    },
+  },
+});
+
+export default firestoreModel;
